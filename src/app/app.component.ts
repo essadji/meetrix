0cb5b78968a5ce619e223a7636855eab114a8305
essadji <info@essadji.com>
2021-11-12 13:48:39 +0100

    pre-pull

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meetrix';
+  active = "welcome";
+  activate(x:any){
+    console.log(x)
+    this.active = x;
+  }
 }
