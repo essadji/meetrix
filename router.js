55da776e528f65343c25ad2bb13b529efdb71190
essadji <info@essadji.com>
2021-12-08 15:47:33 +0100

    mongo + queries

--- router.js
+++ router.js
@@ -1,8 +1,76 @@
 const X = require('express');
+const { MongoClient } = require("mongodb");
+const URI = "mongodb://localhost/";
+const CLIENT = new MongoClient(URI);
+const DB = CLIENT.db('MyProject').collection('Angular');
 const GO = X.Router();
+
+const ALL = async () => {
+    await CLIENT.connect();
+    await CLIENT.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to MongoDB");
+    const result = await DB.find({
+    //  date: {
+    //      $gte: new Date(new Date().setHours(00, 00, 00)),
+    //      $lt: new Date(new Date().setHours(23, 59, 59)),
+    //  },
+    }).toArray();
+    return result;
+}
+const FILTER = async () => {
+    await CLIENT.connect();
+    await CLIENT.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to MongoDB");
+    const result = await DB.find({
+        "lector.kapsel":{$exists:true}
+    }).toArray();
+    return result;
+}
+const ADD = async (x) => {
+    await CLIENT.connect();
+    await CLIENT.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to MongoDB");
+   const insert = await DB.insertMany([{"date":new Date(),"value":x}]);
+ console.log('INSERT =>', insert);
+return(insert)
+}
+
 
 GO.route('/').all((req, res) => {
     res.send("OK !!!!!")
 })
 
+GO.route('/all').get((req,res)=>{
+    ALL()
+    .then(
+        (x)=>{
+            console.log(x)
+            res.send(x)
+        }
+    )
+    .catch(console.error)
+    .finally(()=>CLIENT.close())
+})
+GO.route('/filter').get((req,res)=>{
+    FILTER()
+    .then(
+        (x)=>{
+            console.log(x)
+            res.send(x)
+        }
+    )
+    .catch(console.error)
+    .finally(()=>CLIENT.close())
+})
+GO.route('/add/:param').get((req,res)=>{
+    ADD(req.params.param)
+    .then(
+        (x)=>{
+            console.log(x)
+            res.send(x)
+        }
+    )
+    .catch(console.error)
+    .finally(()=>CLIENT.close())
+})
 module.exports = GO;
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,19 +9,19 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor(private http:HttpClient) {
+  constructor(private http: HttpClient) {
 
-   }
+  }
 
-   string_api() {
-     return this.http.get("http://essadji.be:1999/mongo/string",{responseType:'text'})
-   }
-   local_api() {
-     return this.http.get("http://localhost:2002/data/", { responseType: 'text' })
-   }
-   json_api() {
-     return this.http.get("http://essadji.be:1999/mongo/json",{responseType:'json'})
-   }
+  string_api() {
+    return this.http.get("http://essadji.be:1999/mongo/string", { responseType: 'text' })
+  }
+  local_api() {
+    return this.http.get("http://localhost:2002/data/", { responseType: 'text' })
+  }
+  json_api() {
+    return this.http.get("http://essadji.be:1999/mongo/json", { responseType: 'json' })
+  }
 }
 
 
