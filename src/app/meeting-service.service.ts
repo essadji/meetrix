5e7b26d46ab201a02f11f3da01a7dd9cb80f19a1
essadji <info@essadji.com>
2021-12-15 09:10:02 +0100

    Update meeting-service.service.ts

--- src/app/meeting-service.service.ts
+++ src/app/meeting-service.service.ts
@@ -11,7 +11,6 @@ export class MeetingServiceService {
     { date: '2018-04-12', name: "Examen", topics: 'Corona', attendees: ['Bjarne', 'Robbe', 'Maarten'], commentary: "Algemeen commentaar: goede samenwerking" },
     { date: '2021-11-12', name: "Netwerken", topics: 'Examens', attendees: ['Robbe', 'Jonas'], commentary: "Algemeen commentaar: goed" },
     { date: '2021-11-12', name: "examens juli", topics: 'Examens', attendees: ['Robbe', 'Maarten'], commentary: "Algemeen commentaar: makkelijke examens" },
-    { date: '2021-11-12', name: "Segers", topics: 'Leerkrachten', attendees: ['Jonas', 'Bjarne'], commentary: "Algemeen commentaar: ontslaan" },
     { date: '2021-11-12', name: "Netwerken", topics: 'Examens', attendees: ['Robbe', 'Jonas'], commentary: "Algemeen commentaar: goed" },
     { date: '2021-11-12', name: "Netwerken", topics: 'Examens', attendees: ['Robbe', 'Jonas'], commentary: "Algemeen commentaar: goed" },
     { date: '2021-11-12', name: "Netwerken", topics: 'Examens', attendees: ['Robbe', 'Jonas'], commentary: "Algemeen commentaar: goed" },
